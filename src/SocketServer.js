0efaae99973072f5ad4bf86a3c248a1fb1b0ad5d
John Richardson <johnmaster208@gmail.com>
2018-05-27 23:23:18 -0400

    ...

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -1,8 +1,7 @@
 
 const express = require('express');
 const app = express();
-// const PORT = process.env.PORT || 5000;
-const PORT = 5001;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT);
 const io = require('socket.io').listen(server);
 console.log('SocketIO server listening to requests on port ' + PORT);
